afdfbe80d78be92cca1409e77ed7ef742eb67d0a
deyizhang <deyizhang2003@gmail.com>
2020-07-08 11:28:18 -0400

    Commit

--- lib/body.dart
+++ lib/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'variables.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,21 +14,27 @@ class _HomePageState extends State<HomePage> {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        DropdownButton<String>(
-          value: dropdownvalue,
-          style: TextStyle(color: atlasPavingBlue),
-          onChanged: (String newValue) {
-            setState(() {
-              dropdownvalue = newValue;
-            });
-          },
-          items: <String>['One', 'Two', 'Free', 'Four']
-              .map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
+        Container(
+          width: 200,
+          child: DropdownButton<String>(
+            isExpanded: true,
+            value: dropdownvalue,
+            style: TextStyle(color: atlasPavingBlue, fontSize: 20),
+            onChanged: (String newValue) {
+              setState(() {
+                dropdownvalue = newValue;
+              });
+            },
+            items: <String>['One', 'Two', 'Free', 'Four']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(
+                  value,
+                ),
+              );
+            }).toList(),
+          ),
         ),
         SizedBox(
           width: double.infinity,
