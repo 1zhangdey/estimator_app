80e48f4798abdad74077e54390adcbf73260883f
deyizhang <deyizhang2003@gmail.com>
2020-07-13 13:10:09 -0400

    Commit

--- lib/Asphalt R&R/Asphalt R&R.dart
+++ lib/Asphalt R&R/Asphalt R&R.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
-
+import 'package:estimatorapp/variables.dart';
 
 class asphaltrandr extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Asphalt R&R"),
-        ),
+      appBar: AppBar(
+        backgroundColor: atlasPavingBlue,
+        title: Text("Asphalt R&R"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            child: TextField(
+
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
