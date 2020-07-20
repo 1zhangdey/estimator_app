4444045cdf6c9f85e04c4feb8f2410371a9ef1a6
deyizhang <deyizhang2003@gmail.com>
2020-07-20 13:42:56 -0400

    Commit

--- lib/Asphalt R&R/paving/layerResults.dart
+++ lib/Asphalt R&R/paving/layerResults.dart
@@ -1,3 +1,4 @@
+import 'package:estimatorapp/Asphalt%20R&R/prices/surfaceUnitPrice.dart';
 import 'package:estimatorapp/pageBuilder.dart';
 import 'package:flutter/material.dart';
 
@@ -15,79 +16,92 @@ class layerResults extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return pageBuilder(
-      pageWidth: double.infinity,
-      widget: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Asphalt Removed: " + loadsRemoved.toString() + " Loads",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 25,
+        pageWidth: double.infinity,
+        widget: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Asphalt Removed: " + loadsRemoved.toString() + " Loads",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                  ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Base Layer: " +
-                    ((pavingBase * surfaceArea * 2.45).ceil()).toString() +
-                    " tons",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 25,
+              ],
+            ),
+            Divider(),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Base Layer: " +
+                      ((pavingBase * surfaceArea * 2.45).ceil()).toString() +
+                      " tons",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                  ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Surface Layer: " +
-                    ((pavingsurface * surfaceArea * 2.45).ceil()).toString() +
-                    " tons",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 25,
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Surface Layer: " +
+                      ((pavingsurface * surfaceArea * 2.45).ceil()).toString() +
+                      " tons",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                  ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Total Paving: " +
-                    (((pavingsurface * surfaceArea * 2.45) +
-                                (pavingBase * surfaceArea * 2.45))
-                            .ceil())
-                        .toString() +
-                    " tons",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 25,
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Total Paving: " +
+                      (((pavingsurface * surfaceArea * 2.45) +
+                                  (pavingBase * surfaceArea * 2.45))
+                              .ceil())
+                          .toString() +
+                      " tons",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                  ),
                 ),
+              ],
+            ),
+          ],
+        ),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => surfacePavingUnitPrice(
+                pavingSurfaceTons: (pavingsurface * surfaceArea * 2.45).ceil(),
+                loadsRemoved: loadsRemoved,
+                totalSurfaceArea: surfaceArea,
+                pavingBaseTons: (pavingsurface * surfaceArea * 2.45).ceil(),
               ),
-            ],
-          ),
-        ],
-      ),
-    );
+            ),
+          );
+        });
   }
 }
---
+++ lib/Asphalt R&R/prices/surfaceUnitPrice.dart
@@ -0,0 +1,87 @@
+import 'package:estimatorapp/pageBuilder.dart';
+import 'package:flutter/material.dart';
+import 'package:estimatorapp/variables.dart';
+
+class surfacePavingUnitPrice extends StatelessWidget {
+  surfacePavingUnitPrice(
+      {this.pavingSurfaceTons,
+      this.pavingBaseTons,
+      this.loadsRemoved,
+      this.totalSurfaceArea});
+  int pavingSurfaceTons;
+  double totalSurfaceArea;
+  int pavingBaseTons;
+  int loadsRemoved;
+  double pavingSurfacePricePerTon;
+  double pavingSurfaceTotalPrice;
+
+  @override
+  Widget build(BuildContext context) {
+    return pageBuilder(
+      pageWidth: 600,
+      widget: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Text(
+                  "Surface Layer; Price Per Ton",
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Center(
+                  child: Text(
+                    pavingSurfaceTons.toString() + " Tons",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  onChanged: (text) {
+                    pavingSurfacePricePerTon = double.parse(text);
+                    pavingSurfaceTotalPrice =
+                        pavingSurfacePricePerTon * totalSurfaceArea;
+                    print(pavingSurfaceTotalPrice);
+                  },
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: atlasPavingBlue, width: 3),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Text(
+                  pavingSurfaceTotalPrice.toString(),
+                  style: TextStyle(fontSize: 30, decorationThickness: 4),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
