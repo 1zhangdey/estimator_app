c00f02182b7c2961e8c7362659b7cc03ffc534d4
deyizhang <deyizhang2003@gmail.com>
2020-07-29 13:37:35 -0400

    Commit

--- lib/Home/Dropdownfield1.dart
+++ lib/Home/Dropdownfield1.dart
@@ -15,66 +15,74 @@ class _DownFormState extends State<Homepage> {
   String dropdownvalue;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: EdgeInsets.only(bottom: 50),
-          width: 300,
-          child: DropdownButton<String>(
-            isExpanded: true,
-            hint: Text("What type of job is this?"),
-            value: dropdownvalue,
-            underline: Container(
-              color: atlasPavingBlue,
-              height: 1,
+    return Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("lib/images/background.jpg"),
+              fit: BoxFit.cover,
             ),
-            style: TextStyle(color: atlasPavingBlue, fontSize: 20),
-            onChanged: (String newValue) {
-               setState(() {
-                 dropdownvalue = newValue;
-                 switch(newValue){
-                   case "Asphalt R&R":
-                     Navigator.push(
-                       context,
-                       MaterialPageRoute(
-                         builder: (context) => asphaltrandr()
-                       ),
-                     );
-                     break;
-                   case "Asphalt R&R + Full Granular":
-                     Navigator.push(
-                       context,
-                       MaterialPageRoute(
-                           builder: (context) => asphaltFullGranular()
-                       ),
-                     );
-                     break;
-                   case "Concrete Curb + Sidewalk":
-                     Navigator.push(
-                       context,
-                       MaterialPageRoute(
-                           builder: (context) => concreteCurbPlusSidewalk()
-                       ),
-                     );
-                     break;
-                 }
-               });
-            },
-            items: <String>[
-              "Asphalt R&R",
-              "Asphalt R&R + Full Granular",
-              "Concrete Curb + Sidewalk"
-            ].map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(
-                  value,
-                ),
-              );
-            }).toList(),
           ),
-        ),
-      ],
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(bottom: 50),
+            width: 300,
+            child: DropdownButton<String>(
+              isExpanded: true,
+              hint: Text("What type of job is this?"),
+              value: dropdownvalue,
+              underline: Container(
+                color: atlasPavingBlue,
+                height: 1,
+              ),
+              style: TextStyle(color: atlasPavingBlue, fontSize: 20),
+              onChanged: (String newValue) {
+                 setState(() {
+                   dropdownvalue = newValue;
+                   switch(newValue){
+                     case "Asphalt R&R":
+                       Navigator.push(
+                         context,
+                         MaterialPageRoute(
+                           builder: (context) => asphaltrandr()
+                         ),
+                       );
+                       break;
+                     case "Asphalt R&R + Full Granular":
+                       Navigator.push(
+                         context,
+                         MaterialPageRoute(
+                             builder: (context) => asphaltFullGranular()
+                         ),
+                       );
+                       break;
+                     case "Concrete Curb + Sidewalk":
+                       Navigator.push(
+                         context,
+                         MaterialPageRoute(
+                             builder: (context) => concreteCurbPlusSidewalk()
+                         ),
+                       );
+                       break;
+                   }
+                 });
+              },
+              items: <String>[
+                "Asphalt R&R",
+                "Asphalt R&R + Full Granular",
+                "Concrete Curb + Sidewalk"
+              ].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(
+                    value,
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
